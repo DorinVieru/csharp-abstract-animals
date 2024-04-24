946ebbcd34df104914e6d20786a22ac69e7d3834
DorinVieru <dorino.vieru@gmail.com>
2024-04-24 14:44:23 +0200

    UP: Write methods for IVolare and INuotante

--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -2,6 +2,15 @@
 {
     internal class Program
     {
+        public static void FaiVolare(IVolante animale)
+        {
+            animale.Vola();
+        }
+
+        public static void FaiNuotare(INuotante animale)
+        {
+            animale.Nuota();
+        }
 
         static void Main(string[] args)
         {
