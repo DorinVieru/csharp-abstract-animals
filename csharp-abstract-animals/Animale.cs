b7953a96294b0f62b97714f6c4f6ea928464c92f
DorinVieru <dorino.vieru@gmail.com>
2024-04-24 14:35:35 +0200

    UP: write class for different animals

--- csharp-abstract-animals/Animale.cs
+++ csharp-abstract-animals/Animale.cs
@@ -16,4 +16,60 @@ namespace csharp_abstract_animals
         public abstract void Verso();
         public abstract void Mangia();
     }
+
+    // Classe Cane
+    public class Cane : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Bau Bau Bau");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangia carne e verdure");
+        }
+    }
+
+    // Classe Passerotto
+    public class Passerotto : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Twit Twit");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangia semi/cereali");
+        }
+    }
+
+    // Classe Aquila
+    public class Aquila : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Un verso che acusticamente è intenso ed è molto acuto");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangia carne");
+        }
+    }
+
+    // Classe Delfino
+    public class Delfino : Animale
+    {
+        public override void Verso()
+        {
+            Console.WriteLine("Ultrasuoni");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine("Mangia pesce");
+        }
+    }
 }
