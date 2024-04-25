b8e9452b78c892af1f2ed0624f9bd866223834ee
DorinVieru <dorino.vieru@gmail.com>
2024-04-25 11:52:07 +0200

    UP: Write different case for different animals. Finish exercise

--- csharp-abstract-animals/Program.cs
+++ csharp-abstract-animals/Program.cs
@@ -22,7 +22,50 @@
                 new Delfino(),
             };
 
-            
+            Console.WriteLine("-------------\nSe desideri uscire dal programma, digita il tasto 9\n-------------");
+            string input = "";
+            while (input != "9")
+            {
+
+                Console.WriteLine("\nScegli un animale (digita il numero corrispondende all'animale): \n 1. Cane, \n 2. Passerotto, \n 3. Aquila, \n 4. Delfino");
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        var cane = (Cane)animali[0];
+                        Console.WriteLine("--------\nHai scelto il Cane\n--------");
+                        cane.Verso();
+                        cane.Mangia();
+                        break;
+                    case "2":
+                        var passerotto = (Passerotto)animali[1];
+                        Console.WriteLine("--------\nHai scelto il Passerotto\n--------");
+                        passerotto.Dormi();
+                        passerotto.Verso();
+                        passerotto.Mangia();
+                        break;
+                    case "3":
+                        var aquila = (Aquila)animali[2];
+                        Console.WriteLine("--------\nHai scelto l'Aquila\n--------");
+                        aquila.Dormi();
+                        aquila.Verso();
+                        aquila.Mangia();
+                        FaiVolare(aquila);
+                        break;
+                    case "4":
+                        var delfino = (Delfino)animali[3];
+                        Console.WriteLine("--------\nHai scelto il Delfino\n--------");
+                        delfino.Dormi();
+                        delfino.Verso();
+                        delfino.Mangia();
+                        FaiNuotare(delfino);
+                        break;
+                    default:
+                        Console.WriteLine("--------\nAnimale non riconosciuto.\n--------");
+                        break;
+                }
+            }
         }
     }
 }
